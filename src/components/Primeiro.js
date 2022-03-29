dbe6bf738391388c9957c3d7dd17487edf5d6786
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2022-03-29 12:20:45 -0400

    aula22 - console,warn

--- src/components/Primeiro.js
+++ src/components/Primeiro.js
@@ -2,4 +2,9 @@ import React from 'react';
 import { Text } from 'react-native';
 import Estilo from './estilo'
 
-export default () => <Text style={Estilo.fontG}>Primeiro Componente!!</Text>
+export default () => {
+    console.warn('Opa!')
+    return(
+        <Text style={Estilo.fontG}>Primeiro Componente!!</Text>
+    )
+}
