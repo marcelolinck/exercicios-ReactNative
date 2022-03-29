33e70fe2acdd00b9632f8babc60806ae8bed9abe
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2022-03-28 22:46:51 -0400

    aula18

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-function App(){
-    return <Text>Primeiro Componente</Text>
-}
+// function App(){
+//     return <Text>Primeiro Componente</Text>
+// }
+// -----------------------
+//Componente montado em cima e uma constante
+// const App = function(){
+//     return <Text>Componente montado por constante</Text>
+// }
 
-export default App
+// export default App
+// -----------------------
+// export default () =>{
+//     return <Text>Componente montado com arrow function</Text>
+// }
+
+export default () => <Text>Componente montado com arrow function com simplificacao</Text>
