20a5e72063329b6154854b06815a03079da4be28
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2022-04-03 22:20:56 -0400

    aula26

--- src/components/Aleatorio.js
+++ src/components/Aleatorio.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Text } from 'react-native';
 import estilo from './estilo';
 
-export default props => {
-    const delta = props.max - props.min + 1;
-    const aleatorio = parseInt(Math.random() * delta) + props.min;
+// Desta forma o parametro vem via props e podemos deixar o destructor protegido
+export default ({ min, max }) => {
+    const delta = max - min + 1;
+    const aleatorio = parseInt(Math.random() * delta) + min;
     return(
         <Text style={estilo.txtG}>
             O valor aleatorio é {aleatorio}
